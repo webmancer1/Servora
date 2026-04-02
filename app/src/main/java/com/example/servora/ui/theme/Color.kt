22bd0abd282e8cc51d6019fced6dbefb88c96035
webmancer1 <mwendwajames2004@gmail.com>
2026-04-02 14:53:13 +0300

    UI changes

--- app/src/main/java/com/example/servora/ui/theme/Color.kt
+++ app/src/main/java/com/example/servora/ui/theme/Color.kt
@@ -2,10 +2,37 @@ package com.example.servora.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val DeepNavy = Color(0xFF0A0E1A)
+val Charcoal = Color(0xFF141824)
+val DarkSurface = Color(0xFF1B2030)
+val CardSurface = Color(0xFF1E2436)
+val CardBorder = Color(0xFF2A3148)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val NeonCyan = Color(0xFF00E5FF)
+val NeonCyanDim = Color(0xFF00B8D4)
+val ElectricBlue = Color(0xFF448AFF)
+
+val MintGreen = Color(0xFF00E676)
+val MintGreenDim = Color(0xFF00C853)
+
+val AmberWarning = Color(0xFFFFB300)
+val AmberWarningDim = Color(0xFFFF8F00)
+
+val CoralRed = Color(0xFFFF5252)
+val CoralRedDim = Color(0xFFD32F2F)
+
+val TextPrimary = Color(0xFFE8EAF0)
+val TextSecondary = Color(0xFF8A8FA8)
+val TextTertiary = Color(0xFF565B73)
+
+val GaugeCpuStart = Color(0xFF00E5FF)
+val GaugeCpuEnd = Color(0xFF448AFF)
+val GaugeMemoryStart = Color(0xFF00E676)
+val GaugeMemoryEnd = Color(0xFF69F0AE)
+val GaugeDiskStart = Color(0xFFFFB300)
+val GaugeDiskEnd = Color(0xFFFFD54F)
+
+val GlowCyan = Color(0x3300E5FF)
+val GlowGreen = Color(0x3300E676)
+val GlowAmber = Color(0x33FFB300)
+val GlowRed = Color(0x33FF5252)
