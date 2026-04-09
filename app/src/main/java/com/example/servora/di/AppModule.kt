befcef62c9112b463b20b1447fcfbfd0e4d9b135
webmancer1 <mwendwajames2004@gmail.com>
2026-04-09 13:12:19 +0300

    app module

--- app/src/main/java/com/example/servora/di/AppModule.kt
+++ app/src/main/java/com/example/servora/di/AppModule.kt
@@ -1,9 +1,15 @@
 package com.example.servora.di
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import com.example.servora.data.repository.AuthRepository
 import com.example.servora.data.repository.ServerRepository
+import com.example.servora.data.repository.authDataStore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -15,5 +21,17 @@ object AppModule {
     @Singleton
     fun provideServerRepository(): ServerRepository {
         return ServerRepository()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return context.authDataStore
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(dataStore: DataStore<Preferences>): AuthRepository {
+        return AuthRepository(dataStore)
     }
 }
